45d3196ab9531f38ef73faf735c4c000923cc3a8
Hadi Moshayedi <hadi@ubicloud.com>
2025-05-02 15:00:28 -0700

    Add github-workflow and link isa-l_crypto statically.

--- build.rs
+++ build.rs
@@ -1,26 +1,21 @@
-// build.rs
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to tell rustc to link the ISA-L Crypto library.
-    println!("cargo:rustc-link-lib=dylib=isal_crypto");
-    println!("cargo:rustc-link-search=/usr/lib");
+    // Link to the static version of isa-l_crypto
+    println!("cargo:rustc-link-lib=static=isal_crypto");
+    println!("cargo:rustc-link-search=native=/usr/lib");
 
     // The header file for ISA-L Crypto
     let header = "/usr/include/isa-l_crypto.h";
 
-    // Generate the bindings.
+    // Generate Rust bindings
     let bindings = bindgen::Builder::default()
         .header(header)
-        // Depending on your needs, you might want to whitelist functions/types:
-        // .whitelist_function("crypto_.*")
-        // .whitelist_type("crypto_.*")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate ISA-L Crypto bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))

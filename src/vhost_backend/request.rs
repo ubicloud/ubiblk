909a986ba580c8066a08c50566f4d1478ab12fa8
Hadi Moshayedi <hadi@ubicloud.com>
2025-06-09 10:01:46 -0700

    Add license information

--- src/vhost_backend/request.rs
+++ src/vhost_backend/request.rs
@@ -1,15 +1,18 @@
 // This file is mostly based on cloud-hypervisor/block/src/lib.rs, which has
 // the following copyright notice and license:
 //
-// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// > Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// >
+// > Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
+// > Use of this source code is governed by a BSD-style license that can be
+// > found in the LICENSE-BSD-3-Clause file.
+// >
+// > Copyright © 2020 Intel Corporation
+// >
+// > SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
 //
-// Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE-BSD-3-Clause file.
-//
-// Copyright © 2020 Intel Corporation
-//
-// SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
+// You can find CloudHypervisor's code at
+// https://github.com/cloud-hypervisor/cloud-hypervisor
 
 use std::result;
 

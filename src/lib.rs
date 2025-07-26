5bc82563772bd10ba7ecdf8ea66904b2fbcc771d
Hadi Moshayedi <hadi@ubicloud.com>
2025-07-26 07:15:29 -0700

    Allow building without isa-l_crypto
    Added optional dependencies and a feature named “disable-isal-crypto” to
    allow using the pure Rust xts-mode implementation instead of
    isa-l_crypto. This is useful for developer environments which don't
    support isa-l_crypto.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(not(feature = "disable-isal-crypto"))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub mod block_device;

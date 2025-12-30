804657244b73a99b9a64936123ffb593d210af88
Hadi Moshayedi <hadi@ubicloud.com>
2025-12-29 21:00:46 -0800

    Add tests for StripeSourceBuilder.

--- src/stripe_source/mod.rs
+++ src/stripe_source/mod.rs
@@ -1,9 +1,4 @@
-use crate::{
-    block_device::{NullBlockDevice, SharedBuffer},
-    stripe_server::{connect_to_stripe_server, PskCredentials, StripeServerClient},
-    vhost_backend::{build_block_device, Options},
-    KeyEncryptionCipher, Result,
-};
+use crate::{block_device::SharedBuffer, Result};
 
 /// A source from which stripes can be fetched.
 pub trait StripeSource {
@@ -18,49 +13,8 @@ pub trait StripeSource {
 }
 
 mod bdev;
+mod builder;
 mod remote;
 pub use bdev::BlockDeviceStripeSource;
+pub use builder::StripeSourceBuilder;
 pub use remote::RemoteStripeSource;
-
-pub struct StripeSourceBuilder {
-    options: Options,
-    kek: KeyEncryptionCipher,
-    stripe_sector_count: u64,
-}
-
-impl StripeSourceBuilder {
-    pub fn new(options: Options, kek: KeyEncryptionCipher, stripe_sector_count: u64) -> Self {
-        Self {
-            options,
-            kek,
-            stripe_sector_count,
-        }
-    }
-
-    pub fn build(&self) -> Result<Box<dyn StripeSource>> {
-        if let Some(remote_image) = &self.options.remote_image {
-            let client = build_remote_client(&self.options, &self.kek, remote_image)?;
-            let stripe_source =
-                RemoteStripeSource::new(Box::new(client), self.stripe_sector_count)?;
-            return Ok(Box::new(stripe_source));
-        }
-
-        let block_device = if let Some(image_path) = &self.options.image_path {
-            build_block_device(image_path, &self.options, self.kek.clone(), true)?
-        } else {
-            NullBlockDevice::new()
-        };
-
-        let stripe_source = BlockDeviceStripeSource::new(block_device, self.stripe_sector_count)?;
-        Ok(Box::new(stripe_source))
-    }
-}
-
-fn build_remote_client(
-    options: &Options,
-    kek: &KeyEncryptionCipher,
-    server_addr: &str,
-) -> Result<StripeServerClient> {
-    let psk = PskCredentials::from_options(options, kek)?;
-    connect_to_stripe_server(server_addr, psk.as_ref())
-}

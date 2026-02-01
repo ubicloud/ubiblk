42f79c7939c6b7083bb1b3e1e4e241274667dc86
Hadi Moshayedi <hadi@ubicloud.com>
2026-02-01 01:48:18 -0800

    Allow using the error_context macro with dynamic return types.

--- crates/ubiblk-macros/src/lib.rs
+++ crates/ubiblk-macros/src/lib.rs
@@ -4,7 +4,7 @@ use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input, parse_quote,
     punctuated::Punctuated,
-    Expr, ItemFn, Token,
+    Expr, ItemFn, ReturnType, Token,
 };
 
 struct ErrorContextArgs {
@@ -30,12 +30,23 @@ pub fn error_context(attr: TokenStream, item: TokenStream) -> TokenStream {
         quote!(format!(#args))
     };
     let mut function = parse_macro_input!(item as ItemFn);
+    let output_type = match &function.sig.output {
+        ReturnType::Type(_, ty) => ty,
+        ReturnType::Default => {
+            return syn::Error::new_spanned(
+                &function.sig.ident,
+                "error_context requires a return type",
+            )
+            .to_compile_error()
+            .into();
+        }
+    };
     let block = function.block;
 
     function.attrs.push(parse_quote!(#[track_caller]));
     function.block = Box::new(parse_quote!({
         let __ubiblk_location = std::panic::Location::caller();
-        let __ubiblk_result: crate::Result<_> = (|| { #block })();
+        let __ubiblk_result = (|| -> #output_type { #block })();
         __ubiblk_result.map_err(|__ubiblk_error| {
             __ubiblk_error.context_at(#message, __ubiblk_location)
         })
